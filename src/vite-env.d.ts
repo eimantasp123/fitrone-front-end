2c4bc92e15158d199d2359139b28b26ad2893c44
eimantasp123 <eimantaspetrikas18@gmail.com>
2025-03-20 00:12:02 +0100

    last

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,6 +6,7 @@ interface ImportMetaEnv {
   readonly VITE_GOOGLE_CLIENT_ID: string;
   readonly VITE_FACEBOOK_APP_ID: string;
   readonly VITE_MODE: "development" | "production";
+  readonly VITE_GOOGLE_MAP_API_KEY: string;
   // Add other environment variables here...
 }
 
