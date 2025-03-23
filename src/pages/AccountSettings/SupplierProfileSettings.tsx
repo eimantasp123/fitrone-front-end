ab858e04493bc9fa2f131b465583876966e4585e
eimantasp123 <eimantaspetrikas18@gmail.com>
2025-03-23 23:52:03 +0100

    last

--- src/pages/AccountSettings/SupplierProfileSettings.tsx
+++ src/pages/AccountSettings/SupplierProfileSettings.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from "react-i18next";
 import EditProfile from "./components/EditProfile";
 import ChangePassword from "./components/ChangePassword";
 import TwoFactorAuth from "./components/TwoFactorAuth";
-import EditBusinessInfo from "./components/EditBusinessInfo";
+// import EditBusinessInfo from "./components/EditBusinessInfo";
 // import DeleteAccount from "./components/DeleteAccount";
 
 const SupplierProfileSettings: React.FC = () => {
@@ -17,10 +17,10 @@ const SupplierProfileSettings: React.FC = () => {
         <h2 className="mt-3 font-semibold lg:mt-8">
           {t("businessInfo.title")}
         </h2>
-        <EditBusinessInfo />
+        {/* <EditBusinessInfo />
         <h2 className="mt-3 font-semibold lg:mt-8">
           {t("changePassword.title")}
-        </h2>
+        </h2> */}
         <ChangePassword />
         <h2 className="mt-3 font-semibold lg:mt-8">{t("2fa.title")}</h2>
         <TwoFactorAuth />
